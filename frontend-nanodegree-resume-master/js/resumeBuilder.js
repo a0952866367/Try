d0b6275ead893551930b4304106a4de7d26c62b8
a0952866367 <a0952866367@users.noreply.github.com>
2016-07-07 16:05:33 +0800

    Add files via upload

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -1,3 +1,17 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Andy Tseng";
+var role = "Student";
+var array = [ "a" , "b" , "c" , "d"];
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var bio = {
+    "name" : name,
+    "role" : role,
+    "contactInfo" : "0939202878",
+    "pictureURL" : images/"197x148.gif"
+};
+
+$("#header").append(bio.pictureURL);
